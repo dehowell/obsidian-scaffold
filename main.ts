72e504aa46ee330b7550759efc959361c81b0385
David Howell <dehowell@gmail.com>
2021-09-23 05:50:44 -0400

    Add function to reformat my old Bear title lines

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Plugin } from 'obsidian';
 
+const BEAR_TITLE_LINE = /^\["(?<title>.*?)", (?<author>.*?), (?<publication>.*?)\]\((?<url>.*?)\)$/;
 export default class DaveScaffoldingPlugin extends Plugin {
 	async onload() {
 		this.addCommand({
@@ -14,6 +15,33 @@ export default class DaveScaffoldingPlugin extends Plugin {
 				let trimmedContent = content.replace(/ +$/gm, "");
 				editor.setValue(trimmedContent);
 				editor.setCursor(position);
+			}
+		});
+
+		this.addCommand({
+			id: 'reformat-bear-title-line',
+			name: 'Reformat Bear Title Line',
+			callback: () => {},
+			editorCallback: (editor, view) => {
+				let position = editor.getCursor();
+				let content = editor.getValue();
+				let lines = content.split("\n");
+				let firstLine = lines.shift();
+				let rest = lines.join("\n");
+				
+				let match = firstLine.match(BEAR_TITLE_LINE);
+				if (match) {
+					let newContent = [
+						`# [${match.groups.title}](${match.groups.url})`,
+						match.groups.author,
+						match.groups.publication,
+						`#refnote #type/literature/article`,
+						rest
+					].join("\n");
+
+					editor.setValue(newContent);
+					editor.setCursor(position);
+				}
 			}
 		});
 	}
